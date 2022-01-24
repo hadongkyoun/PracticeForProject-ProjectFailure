11e1c445524a11dc97b56fdb20d28163c82cad15
DongKyoun <gkehdrbs36@naver.com>
2022-01-24 21:59:15 +0900

    All for practice

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,13 +1,49 @@
 const express = require('express');
-//HTML로 응답하려고 한다.
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 const path = require('path');
 
+dotenv.config();
+//HTML로 응답하려고 한다.
+//const path = require('path');
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req,res)=>{
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, 'publikc')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'session-cookie',
+}));
+//미들웨어
+app.use((req,res,next) =>{
+  console.log("모든 요청에 다 실행 됩니다.");
+  next();
+});
+
+app.get('/', (req,res, next)=>{
   //res.send('Hello, Express');
-  res.sendFile(path.join(__dirname, '/index.html'));
+  console.log('GET / 요청에서만 실행됩니다.');
+  next();
+  //res.sendFile(path.join(__dirname, '/index.html'));
+}, (req, res)=>{
+  throw new Error('에러는 에러 처리 미들웨어로 갑니다');
+});
+
+app.use((err, req, res, next)=>{
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
 app.listen(app.get('port'), ()=>{
