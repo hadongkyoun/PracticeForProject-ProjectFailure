68ac39aa8dcfa77cb9d015d826041cee09d882c1
DongKyoun <gkehdrbs36@naver.com>
2022-01-25 21:00:18 +0900

    image upload application

--- learn-express/app.js
+++ learn-express/app.js
@@ -27,24 +27,40 @@ app.use(session({
   name: 'session-cookie',
 }));
 //미들웨어
-app.use((req,res,next) =>{
-  console.log("모든 요청에 다 실행 됩니다.");
-  next();
+
+const multer = require('multer');
+const fs = require('fs');
+
+try{
+  fs.readdirSync('uploads');
+}
+catch (error){
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done){
+      done(null, 'uploads/');
+    },
+    filename(req, file, done){
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: {fileSize: 5 * 1024 * 1024},
+});
+app.get('/upload', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'multipart. html'));
 });
 
-app.get('/', (req,res, next)=>{
-  //res.send('Hello, Express');
-  console.log('GET / 요청에서만 실행됩니다.');
-  next();
-  //res.sendFile(path.join(__dirname, '/index.html'));
-}, (req, res)=>{
-  throw new Error('에러는 에러 처리 미들웨어로 갑니다');
+app.post('/upload', upload.single('image'), (req,res)=>{
+  console.log(req.file, req.body);
+  res.end('ok');
 });
 
-app.use((err, req, res, next)=>{
-  console.error(err);
-  res.status(500).send(err.message);
-});
+
 
 app.listen(app.get('port'), ()=>{
   console.log(app.get('port'), '번 포트에서 대기 중');
