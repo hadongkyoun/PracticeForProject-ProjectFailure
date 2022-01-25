e87624c36ddb1924f5d6aac193eb3d6f3f7a5201
DongKyoun <gkehdrbs36@naver.com>
2022-01-25 21:00:36 +0900

    image upload application

--- learn-express/app.js
+++ learn-express/app.js
@@ -61,6 +61,19 @@ app.post('/upload', upload.single('image'), (req,res)=>{
 });
 
 
+app.get('/', (req,res, next)=>{
+  //res.send('Hello, Express');
+  console.log('GET / 요청에서만 실행됩니다.');
+  next();
+  //res.sendFile(path.join(__dirname, '/index.html'));
+}, (req, res)=>{
+  throw new Error('에러는 에러 처리 미들웨어로 갑니다');
+});
+
+app.use((err, req, res, next)=>{
+  console.error(err);
+  res.status(500).send(err.message);
+});
 
 app.listen(app.get('port'), ()=>{
   console.log(app.get('port'), '번 포트에서 대기 중');
