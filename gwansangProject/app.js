ca40c226b5c92b508160f26223712254181e8efb
DongKyoun <gkehdrbs36@naver.com>
2022-02-09 11:47:03 +0900

    fix text.html

--- gwansangProject/app.js
+++ gwansangProject/app.js
@@ -11,13 +11,14 @@ const fs = require('fs');
 const { response } = require('express');
 
 
-
+/*upload 폴더가 없는 경우*/
 try{
   fs.readdirSync('public/uploads');
 }catch(error){
   console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
   fs.mkdirSync('public/uploads');
 }
+
 //파일저장 위치
 //const upload = multer({dest: 'public/images/'}) //dest : 저장 위치
 const upload = multer ({
@@ -35,15 +36,14 @@ const upload = multer ({
 
 
 dotenv.config();
-/*const indexRouter = require('./routes');
-const userRouter = require('./routes/user');*/
+
 const app = express();
 
 //기본 주소
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'publikc')));
+app.use('/', express.static(path.join(__dirname, 'publikc'))); //지워도 될거 같음
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -57,27 +57,36 @@ app.use(session({
   },
   name: 'session-cookie',
 }));
-//미들웨어
-
-/*app.use('/', indexRouter);
-app.use('/user', userRouter);*/
-
 
 app.use(express.static('public'));
-// 기본 실행
+
+
+// 관상 페이지
 app.get('/', (req, res)=>{
  console.log('정상 실행');
  res.sendFile(path.join(__dirname, './public/gwansang.html'));
 });
 
-//name이 image로 된 이미지를 받아서 해당 경로에 저장
+// 텍스트 변환 페이지
+app.get('/text.html', (req, res)=>{
+console.log('정상 실행');
+res.sendFile(path.join(__dirname,'./public/text.html'));
+});
+
+
+//프론트에서 준 name이 image로 된 이미지를 받아서 해당 경로에 저장
 app.post('/upload', upload.single('image'), (req,res)=>{
-  //여기서 이미지 변환 후 저장
-  /*이미지를 다시 보여주기
-    post 기능??? 
+  
+
+  /*
+    next('route');
+    ①이미지 변환
+    ②변환 된 이미지를 Client가 다운 받을 수 있도록 프론트로 전송 
   */
+
     res.sendFile(path.join(__dirname, './public/uploads/' + req.file.originalname));
 });
+
 
 
 /*프론트에서 이미지 요청 (app.get으로 주로 처리)
